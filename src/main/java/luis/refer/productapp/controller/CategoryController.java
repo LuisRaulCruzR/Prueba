9790276c4a5c46e07426da3c7a2ccc7484d01fb7
LuisRaulCruzR <83298310+LuisRaulCruzR@users.noreply.github.com>
2022-03-07 07:08:11 -0600

    update

--- src/main/java/luis/refer/productapp/controller/CategoryController.java
+++ src/main/java/luis/refer/productapp/controller/CategoryController.java
@@ -1,12 +1,16 @@
 package luis.refer.productapp.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import luis.refer.productapp.models.Category;
 import luis.refer.productapp.services.CategoryService;
@@ -28,6 +32,18 @@ public class CategoryController {
 	
 	@PostMapping("/categories/addNew")
 	public String addNew(Category category) {
+		categoryService.save(category);
+		return "redirect:/categories";		
+	}
+	
+	@RequestMapping("/categories/findById")
+	@ResponseBody
+	public Optional<Category> findById(int id) {
+	 return	categoryService.findById(id);
+	}
+	
+	@RequestMapping(value="/categories/update", method= {RequestMethod.PUT, RequestMethod.GET})
+	public String update(Category category) {
 		categoryService.save(category);
 		return "redirect:/categories";		
 	}
--- src/main/resources/static/js/category.js
+++ src/main/resources/static/js/category.js
@@ -1,6 +1,19 @@
 $('document').ready(function(){
 	
 	$('table #editButton').on('click',function(event){
+		event.preventDefault();
+		
+		// /categories/findById/?id=1
+		
+		var href = $(this).attr('href');
+		
+		$.get(href, function(category, status){
+			$('#idEdit').val(category.id);
+			$('#descripcionEdit').val(category.descripcion);
+			$('#detalleEdit').val(category.detailes);
+			$('#categoriaEdit').val(category.categoria);
+		});
+		
 		$('#editModal').modal();	
 	});
 });
